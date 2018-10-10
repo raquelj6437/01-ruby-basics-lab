8875a6f9344c562dc2aba09c0316272a136e6cbc
Raquel Joseph <raquelj6437@hstat.org>
2018-10-10 01:03:46 +0000

    pass rules

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(num1, num2)
+    num1/num2
 end
 
-def argue
-  
+def argue(phrase)
+    phrase
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
-def return_a_value
-    
+def return_a_value(value="Nice")
+    value
 end
 
-def pizza_party
-    
+def pizza_party(topping = "cheese")
+    topping
 end
 
-def whisper
-    
+def whisper(phrase)
+    phrase.downcase
 end
